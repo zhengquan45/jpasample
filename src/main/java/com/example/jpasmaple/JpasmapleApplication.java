f903637d53d70a7f5579c1ecb2b0174254b2ea23
郑权 <zheng_quan45@163.com>
2021-03-08 20:08:22 +0800

    first commit

---
+++ src/main/java/com/example/jpasmaple/JpasmapleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpasmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpasmapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpasmapleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpasmaple/JpasmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpasmaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpasmapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
